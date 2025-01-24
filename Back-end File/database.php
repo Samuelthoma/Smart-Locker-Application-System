70037b2d18196b9f50c5216ff3e91803ab18a457
Samuel Thomas <samuel.thomas@student.president.ac.id>
2025-01-24 21:10:10 +0700

    Update full code

--- Back-end File/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-    function dbconnection(){
-        $con = mysqli_connect("localhost", "root", "", "economic");
-        return $con;
-    }
-?>
---
+++ android/app/src/main/kotlin/com/example/eco_apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eco_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
